e9891496ecb753bf3071d3b38c4ff9c0b5e219c2
Lucas Barga <lucasmpbarga@gmail.com>
2023-01-08 01:46:11 -0300

    small adjusts

--- demo01-lambda-without-framework/run.sh
+++ demo01-lambda-without-framework/run.sh
@@ -1,10 +1,9 @@
 # 1. create the policy security files.
-# 2. create security role with IAM
 
+# 2. create security role with IAM
 aws iam create-role --role-name lambda-example --assume-role-policy-document file://policies.json | tee logs/role.log
 
 # 3. create project and zip 
-
 aws lambda create-function --function-name hello-cli --zip-file fileb://index.zip --handler index.handler --runtime nodejs12.x --role arn:aws:iam::215785215210:role/lambda-example | tee logs/lambda-create.log
 
 # 4. invoke lambda!
